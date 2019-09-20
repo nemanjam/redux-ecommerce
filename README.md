f891c14e42d3161359ce518cabcbd01173e7a70a
nemanjam <nemanja.mitic.elfak@hotmail.com>
2019-09-20 21:27:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Eshop-page in progress
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+    return <Header />;
+};
+
+export default Home;
