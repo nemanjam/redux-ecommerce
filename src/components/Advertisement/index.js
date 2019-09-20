9b32f55be1049495241d7e39dba4a50d0e136eda
nemanjam <nemanja.mitic.elfak@hotmail.com>
2019-09-21 01:45:55 +0200

    add advertisement

---
+++ src/components/Advertisement/index.js
@@ -0,0 +1,3 @@
+import Advertisement from './Advertisement';
+
+export default Advertisement;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Product from '../../components/Product';
+import Advertisement from '../../components/Advertisement';
 import SortBy from '../../components/SortBy';
 import './styles.css';
 
@@ -15,7 +16,7 @@ const Home = () => {
         <div className="row">
           <Product />
           <Product />
-          <Product />
+          <Advertisement />
           <Product />
           <Product />
           <Product />
